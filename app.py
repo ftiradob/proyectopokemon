3a7097e5be2027fb1beaaa0cacb13488384acae7
Fernando <fernando.tb.95@gmail.com>
2019-05-23 12:11:47 +0200

    Añadir static

--- app.py
+++ app.py
@@ -1,27 +1,10 @@
 port=os.environ["PORT"]
 
+from flask import Flask, render_template
+app=Flask(__name__)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/',methods=["GET","POST"])
+def inicio():
+	return render_template("index.html")
 
 app.run('0.0.0.0',int(port), debug=True)
