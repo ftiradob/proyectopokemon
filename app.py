cab3d85c7afa9f99e803aa1d303d1e640c704830
Fernando <fernando.tb.95@gmail.com>
2019-05-29 23:55:16 +0200

    Modificcaciones varias

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ URL_BASE="https://pokeapi.co/api/v2/"
 URL_BASETCG="https://api.pokemontcg.io/v1/"
 listainiciales=["bulbasaur","charmander","squirtle","chikorita","cyndaquil","totodile","treecko","chimchar","piplup","snivy","tepig","oshawott","chespin","fennekin","froakie","rowlet","litten","popplio"]
 
+
 def mostrariniciales():
 	listaimagenes=[]
 	for i in listainiciales:
@@ -43,18 +44,28 @@ def mostrarnumeropokedex():
             print("Error en la petición")
     return listapokedex
 
-def muestradescripcion():
+def muestradescripcion(objeto):
     encontrado=False
-    objeto=input("Indique el Pokemon: ")
     r=requests.get(URL_BASE+"item/"+objeto)
     if r.status_code==200:
         doc=r.json()
         for idioma in doc["flavor_text_entries"]:
             if idioma["language"]["name"]=="es" and encontrado==False:
-                print(idioma["text"])
+                descripcion=idioma["text"]
                 encontrado=True
+        return descripcion
     else:
-        print("Error en la petición")
+    	print("Error en la petición")
+
+#def muestracoste():
+#	listacoste=[]
+#    objeto=input("Indique el Pokemon: ")
+#    r=requests.get(URL_BASE+"item/"+objeto)
+#    if r.status_code==200:
+#        doc=r.json()
+#        print(doc["cost"])
+#    else:
+#        print("Error en la petición")
 
 
 @app.route('/',methods=["GET","POST"])
@@ -66,7 +77,8 @@ def inicio():
 
 @app.route('/nivel',methods=["GET","POST"])
 def nivel():
-	return render_template("nivel.html",listaobjetos=zip())
-
+    objeto=requests.form.get("objeto")
+    descripcion=muestradescripcion(objeto)
+    return render_template("nivel.html",descripcion=descripcion)
 
 app.run('0.0.0.0',int(port), debug=True)
